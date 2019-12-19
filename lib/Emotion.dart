5997b82ddefb62981abd707a38ec0b7237acefad
前田幹太 <kanta01m.tyan@gmail.com>
2019-12-19 14:25:59 +0900

    grid_list

--- lib/Emotion.dart
+++ lib/Emotion.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 // sad: 0, normal: 1, happy: 2
 
 class Emotion {
-  const Emotion({
+  Emotion({
     @required this.date,
     @required this.emotion
   }) : assert(date != null),
        assert(emotion != null);
-
   final DateTime date;
   final int emotion;
+  
+  final _weekName = ['', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+
+  String get assetName => emotion == 0 ? 'assets/sad.png' : (emotion == 1 ? 'assets/normal.png' : 'assets/happy.png');
+  int get printDate => date.day;
+  String get printWeek => _weekName[date.weekday];
+  Color get weekColor => date.weekday == 6 ? Colors.lightBlue[600] : (date.weekday == 7 ? Colors.red[500] : Colors.grey[700]);
 }
